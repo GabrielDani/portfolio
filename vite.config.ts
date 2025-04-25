060a4f08ad58c9d77de2af37f40f9898297c9553
Gabriel Dani de Azevedo <gabriel.dani07@gmail.com>
2025-04-25 14:50:06 -0300

    initial vite

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    sourcemap: false,
+    target: "esnext",
+    outDir: "dist",
+    minify: "esbuild",
+  },
+});
