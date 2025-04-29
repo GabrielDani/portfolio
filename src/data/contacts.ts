3025ba3a07e240d9949bed0f89917c82910c475c
Gabriel Dani de Azevedo <gabriel.dani07@gmail.com>
2025-04-29 16:33:24 -0300

    phone number correct

--- src/data/contacts.ts
+++ src/data/contacts.ts
@@ -1,5 +1,5 @@
 export const linkedin =
   "https://linkedin.com/in/gabriel-dani-de-azevedo-a69740288";
 export const github = "https://github.com/GabrielDani";
-export const phone = "(32)99154-4543";
+export const phone = "(32)98424-4803";
 export const email = "gabriel.dani07@gmail.com";
