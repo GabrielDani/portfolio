384e0220093762abfabff364aabed4f6dcfe9398
GabrielDani <gabriel.dani07@gmail.com>
2025-05-02 18:59:18 -0300

    about buttons grid

--- src/pages/About/components/AboutMainButtons.tsx
+++ src/pages/About/components/AboutMainButtons.tsx
@@ -1,17 +1,31 @@
+import { motion } from "framer-motion";
+import { fadeInUp } from "@/components/animations/variants";
 import { ButtonLink } from "@/components/atoms/ButtonLink";
+import { Code, Contact } from "lucide-react";
 import { FaFileDownload } from "react-icons/fa";
 
 export function AboutMainButtons() {
   return (
-    <div className="mt-6 flex flex-wrap gap-4">
-      <ButtonLink href="/projetos" variant="primary" text="Ver Meus Projetos" />
-      <ButtonLink href="/contato" variant="outline" text="Entrar em Contato" />
+    <div className="grid grid-cols-2 gap-4 mt-6 max-w-lg">
+      <ButtonLink
+        href="/projetos"
+        variant="primary"
+        text="Ver Meus Projetos"
+        icon={<Code className="w-4 h-4 text-base" />}
+      />
+      <ButtonLink
+        href="/contato"
+        variant="outline"
+        text="Entrar em Contato"
+        icon={<Contact className="w-4 h-4 text-base" />}
+      />
       <ButtonLink
         href="/Currículo_GabrielDaniAz.pdf"
         text="Baixar Currículo"
         variant="outline"
         download
         icon={<FaFileDownload className="text-base" />}
+        className="col-span-2 justify-center"
       />
     </div>
   );
