24909c2fcc2e31ae278744789b432576dd8aba41
Gabriel Dani de Azevedo <gabriel.dani07@gmail.com>
2025-05-12 16:37:26 -0300

    linkedin url changed

--- src/data/contacts.ts
+++ src/data/contacts.ts
@@ -1,5 +1,4 @@
-export const linkedin =
-  "https://linkedin.com/in/gabriel-dani-de-azevedo-a69740288";
+export const linkedin = "https://linkedin.com/in/gabrieldaniaz";
 export const github = "https://github.com/GabrielDani";
 export const phone = "(32)98424-4803";
 export const email = "gabriel.dani07@gmail.com";
